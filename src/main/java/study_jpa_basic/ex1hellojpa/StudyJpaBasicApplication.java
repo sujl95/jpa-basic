469a017fdfb963e29ac587b67dbb57966262e5c5
sujl95 <sujl95@naver.com>
2021-04-20 23:25:14 +0900

    init JPA 소개

---
+++ src/main/java/study_jpa_basic/ex1hellojpa/StudyJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package study_jpa_basic.ex1hellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJpaBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study_jpa_basic/ex1hellojpa/StudyJpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study_jpa_basic.ex1hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJpaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
