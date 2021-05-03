ede7e440219b4c8d0a3b7ee60ab05830242635b9
sujl95 <sujl95@naver.com>
2021-05-03 23:31:33 +0900

    Hello JPA - 애플리케이션 개발

--- src/main/java/study_jpa_basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/study_jpa_basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -2,17 +2,28 @@ package study_jpa_basic.ex1hellojpa.hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // DB연결이 다 된다
 
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = emf.createEntityManager(); // createEntityManager 를 꺼내야한다
+		EntityTransaction tx = em.getTransaction(); // transaction 을 얻는다
+		tx.begin(); // 트랜잭션 시작
 
 
-
-		em.close();
+		try {
+			/**
+			 * 코드 작성
+			 */
+			tx.commit(); // 커밋 해주어야 한다.
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
 		emf.close();
 	}
 }
---
+++ src/main/java/study_jpa_basic/ex1hellojpa/hellojpa/Member.java
@@ -0,0 +1,36 @@
+package study_jpa_basic.ex1hellojpa.hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+	@Id
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Member{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
