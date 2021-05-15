7f9284e21b7cda71633dcafdbb277c4ea8e36620
sujl95 <sujl95@naver.com>
2021-05-15 16:42:52 +0900

    연관관계 매핑 기초 - 실전 예제 2 - 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,8 +5,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import jpabook.jpashop.domain.Order;
-
 public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
