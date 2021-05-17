2bf60152c4057c58e3f8f5149f2f95268ade1d15
sujl95 <sujl95@naver.com>
2021-05-17 12:02:35 +0900

    고급 매핑 - 실전 예제 4 - 상속관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import jpabook.jpashop.domain.Book;
+
 public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -15,6 +17,12 @@ public class JpaMain {
 
 
 		try {
+			Book book = new Book();
+			book.setName("JPA");
+			book.setAuthor("김영한");
+			
+			em.persist(book);
+
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
