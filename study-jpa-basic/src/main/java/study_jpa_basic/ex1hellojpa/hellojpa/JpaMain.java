1141c45d6c28cc12587030b7ec0d8864f7fad788
sujl95 <sujl95@naver.com>
2021-05-18 01:47:47 +0900

    프록시와 연관관계 관리 - 프록시

--- study-jpa-basic/src/main/java/study_jpa_basic/ex1hellojpa/hellojpa/JpaMain.java
+++ study-jpa-basic/src/main/java/study_jpa_basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -1,11 +1,11 @@
 package study_jpa_basic.ex1hellojpa.hellojpa;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 	public static void main(String[] args) {
@@ -17,21 +17,37 @@ public class JpaMain {
 
 
 		try {
-			Member member = new Member();
-			member.setUsername("user1");
-			member.setCreatedBy("Lee");
-			member.setCreatedDate(LocalDateTime.now());
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			em.persist(member1);
 
 			em.flush();
 			em.clear();
 
+			Member refMember = em.getReference(Member.class, member1.getId());
+			System.out.println("refMember.getClass() = " + refMember.getClass());
+			Hibernate.initialize(refMember);
+
+
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
+			e.printStackTrace();
 		} finally {
 			em.close();
 		}
 		emf.close();
+	}
+
+	private static void printMember(Member member) {
+		System.out.println("member.getUsername() = " + member.getUsername());
+	}
+
+	private static void printMemberAndTeam(Member member) {
+		String username = member.getUsername();
+		System.out.println("username = " + username);
+
+		Team team = member.getTeam();
+		System.out.println("team.getName() = " + team.getName());
 	}
 }
