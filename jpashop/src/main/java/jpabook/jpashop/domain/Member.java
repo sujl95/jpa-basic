17d9360656b855698eb958d114b92795b564f805
sujl95 <sujl95@naver.com>
2021-05-20 01:04:53 +0900

    값 타입 - 실전 예제 6 - 값 타입 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,9 +19,9 @@ public class Member extends BaseEntity{
 
 	@Column(length = 10)
 	private String name;
-	private String city;
-	private String street;
-	private String zipcode;
+
+	@Embedded
+	private Address address;
 
 	@OneToMany(mappedBy = "member")
 	private List<Order> orders = new ArrayList<>();
@@ -41,27 +42,19 @@ public class Member extends BaseEntity{
 		this.name = name;
 	}
 
-	public String getCity() {
-		return city;
+	public List<Order> getOrders() {
+		return orders;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
-	public String getStreet() {
-		return street;
+	public Address getAddress() {
+		return address;
 	}
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 }
